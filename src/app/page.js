57630ee6e6042cdd29e0b6ba259f59f897317b87
sobahus <sobahusn27@gmail.com>
2025-05-18 23:55:06 +0700

    add alert message

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import HeroSection from "@/components/HeroSection";
 import TechStack from "@/components/TechSkills";
 import _preview from "@/components/Projects/_preview";
 import Footer from "@/components/Footer";
+import Alert from "@/components/Alert";
 
 const Home = () => {
   return (
     <>
+      <Alert />
       <HeroSection />
       <_preview />
       <TechStack />
---
+++ src/components/Alert.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Alert = () => {
+  return (
+    <div className="fixed top-0 bg-red-500 text-red-50 w-60 rounded-b-xl translate-x-20 shadow-sm h-12 flex justify-center items-center">
+      <h2>Projects in Development</h2>
+    </div>
+  );
+};
+
+export default Alert;
+
